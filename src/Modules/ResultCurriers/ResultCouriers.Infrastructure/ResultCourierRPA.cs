771476692c726e0339c6bf225a0a3f0e5a88d623
Juan Camilo Perez <juancamiloperez2009@hotmail.com>
2025-10-23 17:43:23 -0500

    Se agrega gestion de variable de entorno

--- src/Modules/ResultCurriers/ResultCouriers.Infrastructure/ResultCourierRPA.cs
+++ src/Modules/ResultCurriers/ResultCouriers.Infrastructure/ResultCourierRPA.cs
@@ -1,4 +1,5 @@
-﻿using ResultCouriers.Application.Dtos;
+﻿using Microsoft.Extensions.Configuration;
+using ResultCouriers.Application.Dtos;
 using ResultCouriers.Application.Interfaces;
 using System.Net.Http.Json;
 
@@ -9,16 +10,22 @@ namespace ResultCouriers.Infrastructure
     public class ResultCourierRPA:IResultCourierRPA
     {
         private readonly HttpClient _httpClient;
-        public ResultCourierRPA(HttpClient httpClient)
+        private readonly IConfiguration _configuration;
+        public ResultCourierRPA(HttpClient httpClient , IConfiguration configuration)
         {
             _httpClient = httpClient;
+            _configuration = configuration;
         }
         public async Task<ResultCourierResponseRPA> SendCallAsync(SendResultCourierRPADto command)
             {
                
-                var endpointUrl = "http://localhost:3000/ejecutar-comando";
+                var endpointUrl = _configuration["RPA:EndpointUrl"];
+            if (string.IsNullOrEmpty(endpointUrl))
+            {
+                throw new InvalidOperationException("La clave 'RPA:EndpointUrl' no fue encontrada o está vacía en la configuración.");
+            }
 
-                using var response = await _httpClient.PostAsJsonAsync(endpointUrl, command);
+            using var response = await _httpClient.PostAsJsonAsync(endpointUrl, command);
 
      
             string jsonString = await response.Content.ReadAsStringAsync();
