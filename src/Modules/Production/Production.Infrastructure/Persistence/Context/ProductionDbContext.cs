f1234d14cc54e29c992d300935e4ad2423f03f7a
Esteban Montoya Herrera <programacion4@laminaire.net>
2025-11-05 12:22:44 -0500

    Update Production

---
+++ src/Modules/Production/Production.Infrastructure/Persistence/Context/ProductionDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Logistics.Infrastructure.Persistence.Context
+{
+    public class LogisticsDbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public LogisticsDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("LogisticsLaminaireConnection")!;
+        }
+
+        public IDbConnection CreateConnection => new SqlConnection(_connectionString);
+    }
+}
